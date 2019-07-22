78ecd0bc7bd0f997b1f095c2ba87b93ad73b894a
nawelselmi <52922144+nawelselmi@users.noreply.github.com>
2019-07-22 12:04:39 +0200

    Add files via upload

---
+++ gomycode/js/main.js
@@ -0,0 +1,15 @@
+function bold(){
+    document.getElementById("text").style.fontWeight="bold";
+}
+function italic(){
+    document.getElementById("text").style.fontStyle="italic";
+}
+function underline(){
+    document.getElementById("text").style.textDecoration="underline";
+}
+function liste1(){
+    document.getElementById("text").style.fontSize=document.getElementById("v").value;
+}
+function liste2(){
+    document.getElementById("text").style.fontFamily=document.getElementById("n").value;
+}
